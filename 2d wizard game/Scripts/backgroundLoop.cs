8c38876f98de74ac0f5ca62dec91a4733ffd1bf3
moryumi <isikirem00@gmail.com>
2021-05-07 01:41:27 +0300

    initial

---
+++ 2d wizard game/Scripts/backgroundLoop.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundLoop : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 screenBounds;
+    private Camera mainCamera;
+    public List<GameObject> sprites;
+    public Vector2 ScreenBounds
+    {
+        get { return screenBounds; }
+        set { screenBounds = value; }
+    }
+
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,mainCamera.transform.position.z));
+        //Debug.Log("screenbounds"+screenBounds);
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < sprites.Count; i++)  //foreachte error verdi.looptayken listeden remove yaptığın için
+        {
+            repositionBackground(sprites[i]);
+        }
+    }
+
+    public void repositionBackground(GameObject obj)
+    {
+        if (sprites.Count>1)
+        {
+            GameObject firstChild = sprites[0].gameObject;
+            GameObject lastChild = sprites[sprites.Count-1].gameObject;
+            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
+            if (transform.position.x + screenBounds.x > lastChild.transform.position.x + halfObjectWidth)//sağagidiyosa
+            {
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjectWidth * 2,lastChild.transform.position.y,lastChild.transform.position.z);
+                sprites.Add(firstChild);
+                sprites.RemoveAt(0);
+            }
+            else if (firstChild.transform.position.x-halfObjectWidth>transform.position.x-screenBounds.x)//sola gidiyosa
+            {
+                lastChild.transform.position = new Vector3(firstChild.transform.position.x-halfObjectWidth*2,firstChild.transform.position.y,firstChild.transform.position.z);
+                sprites.Insert(0, lastChild); // add lastchild at 0.element
+                sprites.RemoveAt(sprites.Count - 1);
+            }
+        }
+    }
+}
---
+++ 2d wizard game/Scripts/movement.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class movement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private bool collideWithGround;
+    private bool dead,finish;
+    public Camera gameCamera;
+    public backgroundLoop refScript;
+    private bool hey2 = true;
+    public static movement Instance { get; private set; }
+    [SerializeField]
+    private int force;
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        dead = false;
+        finish = false;
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        boxCollider.isTrigger = false;
+    }
+
+    void Update()
+    {
+       // hey2=GameManager.Instance.Hey;
+       // Debug.Log(hey2);
+
+        if (!finish)
+        {
+            if (!dead)
+            {
+                if (Input.GetAxis("Horizontal") > 0)
+                {
+                    transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * Mathf.Lerp(0, 7000, 0.001f);
+                }
+                if (Input.GetAxis("Horizontal") < 0)
+                {
+                    transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * Mathf.Lerp(0, 7000, 0.001f);
+                }
+                if (Input.GetAxis("Vertical") > 0)
+                {
+                    transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * Mathf.Lerp(0, 5000, 0.001f);
+                }
+
+                if (transform.position.x + 10 >= refScript.ScreenBounds.x / 2)
+                {
+                    gameCamera.transform.position = new Vector3(Mathf.Lerp(gameCamera.transform.position.x, transform.position.x, .5f), gameCamera.transform.position.y, gameCamera.transform.position.z);
+                }
+            }
+        }
+        if (transform.position.y < -13f)
+        {
+            GameManager.Instance.GameOver();
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "collider")
+        {
+           // Debug.Log("collideEnter");
+            collideWithGround = true;
+        }
+        else if (collision.gameObject.tag == "moveablecollider")
+        {
+           // Debug.Log("moveable");
+            gameObject.transform.SetParent(collision.transform.parent);
+            
+        }
+        else if (collision.gameObject.tag == "testere" || collision.gameObject.tag == "diken")
+        {
+            dead = true;
+            GameManager.Instance.GameOver();
+            GameOverForce();
+        }
+        else if (collision.gameObject.tag == "door_open")
+        {
+            collision.transform.GetChild(0).gameObject.SetActive(false);
+            collision.transform.GetChild(1).gameObject.SetActive(true);
+            finish = true;
+            GameManager.Instance.FinishLevel(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "collider")
+        {
+            //Debug.Log("collideExit");
+            collideWithGround = false;
+        }
+        else if (collision.gameObject.tag == "moveablecollider")
+        {
+            //Debug.Log("moveable");
+            gameObject.transform.SetParent(null);
+        }
+        
+    }
+
+    public void GameOverForce()
+    {
+        rb.AddForce(new Vector2(-.5f,10) * Time.deltaTime * force);
+        boxCollider.isTrigger = true;
+    }
+}
